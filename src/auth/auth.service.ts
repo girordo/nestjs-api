03588487a9b93c5ecf4ddf4d1ca1d4b1fedec667
Tarcísio Giroldo <tsgiroldo@gmail.com>
2022-05-17 12:42:52 -0300

    :closed_lock_with_key: | Updating Auth service strategy

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,10 +4,15 @@ import { AuthDto } from './dto';
 import * as argon from 'argon2';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable({})
 export class AuthService {
-  constructor(private prisma: PrismaService, private jwt: JwtService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
 
   async signUp(dto: AuthDto) {
     const hash = await argon.hash(dto.password);
@@ -18,9 +23,7 @@ export class AuthService {
           hash,
         },
       });
-
-      delete user.hash;
-      return user;
+      return this.signToken(user.id, user.email);
     } catch (error) {
       if (error instanceof PrismaClientKnownRequestError) {
         if (error.code === 'P2002') {
@@ -47,16 +50,25 @@ export class AuthService {
       throw new ForbiddenException('Invalid email or password');
     }
 
-    delete user.hash;
-    return user;
+    return this.signToken(user.id, user.email);
   }
 
-  async signToken(userId: number, email: string) {
+  async signToken(
+    userId: number,
+    email: string,
+  ): Promise<{ access_token: string }> {
     const payload = {
       sub: userId,
       email,
     };
 
-    return this.jwt.signAsync(payload, { expiresIn: '15m', secret: '' });
+    const secret = this.config.get('JWT_SECRET');
+    const token = await this.jwt.signAsync(payload, {
+      expiresIn: '15m',
+      secret: secret,
+    });
+    return {
+      access_token: token,
+    };
   }
 }
