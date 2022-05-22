3bf0e0b580c86cbe1fcd39ccd4e790a206f18260
Tarcísio Giroldo <tsgiroldo@gmail.com>
2022-05-22 07:30:22 -0300

    :lock: | Updating security using helmet

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,12 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
+  app.enableCors();
+  app.use(helmet());
+  app.use(helmet.noSniff());
+  app.use(helmet.hidePoweredBy());
+  app.use(helmet.contentSecurityPolicy());
+
   await app.listen(3000);
 }
 bootstrap();
